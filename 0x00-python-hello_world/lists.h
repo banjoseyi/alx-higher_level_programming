3918d99dba2770b81d69df0ea657413cbfc14fdf
banjoseyi <banjoseyi2@gamil.com>
2022-11-28 06:19:25 -0800

    my first python tasks

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTs_H
-#define  LISTS_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdlib.h>
 
@@ -10,7 +10,7 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n );
+listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
