2b8008dd274feaa439d7416e726c3598b8093ba7
banjoseyi <banjoseyi2@gamil.com>
2023-03-18 18:45:48 -0700

    object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a script that prints the first State object
+from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
